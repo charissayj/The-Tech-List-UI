766143c307ab1cce4330f8eb2bb1ef32224b5018
Haley <30808485+hdemos@users.noreply.github.com>
2019-10-24 13:52:23 -0500

    Start of Landing UI (#1)
    * basic landing card layout
    
    * landing card with user api
    
    * add Landing
    
    * fix landing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Figures from './components/Figures.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        figures: []
+    };
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        //     fetch('http://localhost:3001/list')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ figures: data })
+            })
+            .catch(console.log)
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>The Tech List - Influential Figures in Tech</h1>
+                    <Figures figures={this.state.figures} />
+                </header>
+
+            </div>
+        );
+    }
+
 }
 
 export default App;
---
+++ src/components/Figures.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Figures = ({ figures }) => {
+    return (
+        <div>
+
+            {figures.map((figure) => (
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{figure.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{figure.email}</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">{figure.data}</h6>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+};
+
+export default Figures
