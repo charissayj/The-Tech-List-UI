ee2fdf20f4d82035ecc7f3e043a991213d8dc46a
Charissa Johnson <charissa.y.johnson@gmail.com>
2019-10-28 23:02:24 -0500

    fix errors for pushing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
-import Figures from './components/Figures.js';
+import React, { Component } from "react";
+import "./App.css";
+import Details from "./components/Details";
+import Figures from "./components/Figures.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
-    state = {
-        figures: []
-    };
+  state = {
+    figures: []
+  };
 
-    componentDidMount() {
-        fetch(process.env.REACT_APP_API_URL + 'list')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ figures: data.peopleArray })
-            })
-            .catch(console.log)
-    }
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>The Tech List - Influential Figures in Tech</h1>
-                </header>
-                <Figures className="figure-list" figures={this.state.figures} />
-            </div>
-        );
-    }
-
+  componentDidMount() {
+    fetch(process.env.REACT_APP_API_URL + "list")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ figures: data.peopleArray });
+      })
+      .catch(console.log);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>The Tech List - Influential Figures in Tech</h1>
+        </header>
+        {/* <Figures className="figure-list" figures={this.state.figures} /> */}
+        <Switch>
+          <Route exact path="/" component={Figures}>
+            <Figures className="figure-list" figures={this.state.figures} />
+          </Route>
+          <Route exact path="/details" component={Details} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Figures.js
+++ src/components/Figures.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Figures = ({ figures }) => {
-    return (
-        <div>
-
-            {figures.map((figure) => (
-                <div class="card" key={figure.id}>
-					<img class="card-img figure-img" src={process.env.REACT_APP_API_URL + figure.imgUrl} alt="Card image"></img>
-                    <div class="card-img-overlay d-flex flex-column justify-content-end">
-						<h5 class="card-title text-white text-shadow">{figure.full_name}</h5>
-                        <h6 class="card-subtitle mb-2 text-white text-shadow">{"Born " + figure.birth_year}</h6>
-                    </div>
-                </div>
-            ))}
-        </div>
-    )
+  const getParams = async person => {
+    const response = await axios.get(`/api/details/${person}`);
+    console.log(response);
+  };
+  return (
+    <div>
+      {figures.map(figure => (
+        <Link to={`/details/${figure.full_name}`}>
+          <div className="card" key={figure.id}>
+            <img
+              className="card-img figure-img"
+              src={process.env.REACT_APP_API_URL + figure.imgUrl}
+              alt="Card"
+            />
+            <div className="card-img-overlay d-flex flex-column justify-content-end">
+              <h5 className="card-title text-white text-shadow">
+                {figure.full_name}
+              </h5>
+              <h6 className="card-subtitle mb-2 text-white text-shadow">
+                {"Born " + figure.birth_year}
+              </h6>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
-export default Figures
+export default Figures;
